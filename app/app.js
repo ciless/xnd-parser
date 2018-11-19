e6777f4439cf5c53b593ff864cd1b70d8361f323
ciless <ciless.it@gmail.com>
2018-11-19 18:38:53 +0300

    connect modules & run app

--- app/app.js
+++ app/app.js
@@ -1,3 +1,23 @@
 import connectorsInit from './connectors';
+import watcher from './helpers/watcher';
+import parser from './helpers/parser';
+import handler from './helpers/handler';
+
+import { CONTRACTS } from './config';
 
 connectorsInit();
+
+async function parse(path) {
+  const data = await parser.parse(path);
+  const handleData = await handler.treatment(data);
+  return handleData;
+}
+
+watcher.watch(CONTRACTS, (event, contractsName, contractsPath) => {
+  console.log(`[${event}] (${contractsName}) - ${contractsPath}`);
+  if (event === 'change') {
+    parse(contractsPath).then((data) => {
+      console.log(data);
+    });
+  }
+});
